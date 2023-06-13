750a0f63f61a1fcd6dcf1c5d3618e5abdedd5531
master <master@SMLAB306PC06_23>
2023-06-12 21:03:35 -0500

    Agregar archivos de proyecto.

---
+++ dNazarenoS3/Page2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace dNazarenoS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string dato1, Double dato2)
+        {
+            InitializeComponent();
+            lblDatoUno.Text = "Dato Uno" + dato1;
+            lblDatoDos.Text = "Dato Dos" + dato2.ToString();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
